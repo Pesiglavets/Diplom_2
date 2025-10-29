ee4aee6e44252d27033541e921805a4e4a5e8ec7
Pesiglavets <russdebut8@gmail.com>
2025-10-29 23:13:09 +0300

    added test_user_creation and its helpers

---
+++ helpers.py
@@ -0,0 +1,50 @@
+import requests
+from faker import Faker
+
+fake = Faker()
+
+BASE_URL = "https://stellarburgers.education-services.ru/api"
+
+def generate_random_email():
+    return fake.email()
+
+def generate_random_password():
+    return fake.password(length=8)
+
+def generate_random_name():
+    return fake.first_name()
+
+def generate_user_data():
+    email = generate_random_email()
+    password = generate_random_password()
+    name = generate_random_name()
+    
+    return email, password, name
+
+def register_new_user():
+    email, password, name = generate_user_data()
+    payload = {"email": email, "password": password, "name": name}
+    response = requests.post(f'{BASE_URL}/auth/register', data=payload)
+    
+    if response.status_code == 200:
+        return email, password, name
+    elif response.status_code == 403:
+        email, password, name = generate_user_data()
+        payload = {"email": email, "password": password, "name": name}
+        response = requests.post(f'{BASE_URL}/auth/register', data=payload)
+        
+        if response.status_code == 200:
+            return email, password, name 
+    return None
+
+def login_user(email, password):
+    payload = {"email": email, "password": password}
+    response = requests.post(f'{BASE_URL}/auth/login', data=payload)
+    return response
+
+def delete_user(auth_token):
+    if auth_token:
+        headers = {'Authorization': auth_token}
+        response = requests.delete(f'{BASE_URL}/auth/user', headers=headers)
+        return response
+    return None
